5dfe35389c1e61de8be0628214cc581c4160c2fc
Guled Ahmed <akindnesstoremember@gmail.com>
2021-02-09 20:57:49 -0800

    added code in the routes folder and in the server file

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const workout = require("./models");
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,23 +13,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
 
-app.use(require("./routes/api-routes.js"));
-
-app.post("/workout", ({ body }, res) => {
-  Workout.create(body)
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
